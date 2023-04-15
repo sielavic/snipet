b4f46d317dcec81731c0b5edfb0202389090aadb
sielavic <63396872+sielavic@users.noreply.github.com>
2023-04-15 13:56:15 +0300

    Update snipet.js

--- snipet.js
+++ snipet.js
@@ -1,4 +1,5 @@
 
+
 var select = document.querySelector("select");
 
 document.querySelector("select").addEventListener('change', function (e) {
@@ -19,15 +20,29 @@ document.querySelector("select").addEventListener('change', function (e) {
     fields.forEach(function(field) {
         
         if ('1' === selectedType) {
-            field.value = 'первый тип полей'
+            if(field.name == 'input_1'||field.name == 'input_2'||field.name == 'input_3'||field.name == 'input_4'||field.name == 'input_5' ||field.name == 'input_6'||field.name == 'input_7'){
+              field.value = 'первый тип полей'
+            }
+            
         }  if ('2' === selectedType) {
+          if(field.name == 'input_1'||field.name == 'input_2'||field.name == 'input_3'||field.name == 'input_4'||field.name == 'input_5' ||field.name == 'input_6'||field.name == 'input_7'){
             field.value = 'второй тип полей'
+          }
         } if ('3' === selectedType) {
-            field.value = 'третий тип полей'
+          if(field.name == 'input_1'||field.name == 'input_2'||field.name == 'input_3'||field.name == 'input_4'||field.name == 'input_5' ||field.name == 'input_6'||field.name == 'input_7'){
+           field.value = 'третий тип полей'
+          }
+            
         } if ('4' === selectedType) {
-            field.value = 'четвертый тип полей'
+          if(field.name == 'input_1'||field.name == 'input_2'||field.name == 'input_3'||field.name == 'input_4'||field.name == 'input_5' ||field.name == 'input_6'||field.name == 'input_7'){
+           field.value = 'четвертый тип полей'
+          }
+            
         } if ('5' === selectedType) {
-            field.value = 'пятый тип полей'
+          if(field.name == 'input_1'||field.name == 'input_2'||field.name == 'input_3'||field.name == 'input_4'||field.name == 'input_5' ||field.name == 'input_6'||field.name == 'input_7'){
+           field.value = 'пятый тип полей'
+          }
+            
         }
     });
 });
